3f6a51c6756cff526cb2bc8987f499c00bd5fdb9
M-msi <blackbunny@gmail.com>
2026-07-13 23:54:58 +0300

    default playlist

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DEFAULT_PLAYLIST_URL: str = "https://www.youtube.com/playlist?list=PLrKoDCoQ3iH5hV6LgXzPz6v-N5d4n7L1A"
+    DEFAULT_PLAYLIST_URL: str = "https://www.youtube.com/watch?v=xgJWXFW1vVc&list=PLVFU_4LbUwis"
     HOST: str = "0.0.0.0"
     PORT: int = 3030
     VLC_ARGS: str = "--no-video --quiet --no-xlib"
